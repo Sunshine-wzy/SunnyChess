33e195244a288096c7cb1b53cfd8b9c0f7012027
Sunshine-wzy <sunshinewzy@qq.com>
2023-06-07 22:14:34 +0800

    Add GoChessBoard

---
+++ GoChessBoard.h
@@ -0,0 +1,12 @@
+#ifndef SUNNYCHESS_GOCHESSBOARD_H
+#define SUNNYCHESS_GOCHESSBOARD_H
+
+#include "ChessBoard.h"
+
+
+class GoChessBoard : public ChessBoard {
+
+};
+
+
+#endif //SUNNYCHESS_GOCHESSBOARD_H
